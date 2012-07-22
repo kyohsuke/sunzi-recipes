78aa2186c15ccae38a598fda1b35f468a692cc96
Keisuke Kawahara <kyohsuke@conafie.jp>
2012-07-22 18:41:23 +0900

    Tweak

---
+++ debian/makeinstall/miruo.sh
@@ -0,0 +1,15 @@
+# miruo
+
+if [ -f /usr/local/sbin/miruo ]; then
+  echo 'mirul already installed'
+else
+  mkdir miruo_build_temp
+  cd miruo_build_temp
+  aptitude -y install libpcap0.8-dev
+  wget 'http://downloads.sourceforge.net/project/miruo/miruo-0.9.3.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmiruo%2Ffiles%2F&ts=1342944918&use_mirror=jaist' -O miruo-0.9.3.tar.gz
+  tar xvzf miruo-0.9.3.tar.gz
+  cd miruo-0.9.3
+  ./configure && make && make install
+  cd ../,,/
+  rm -rf ./miruo_build_temp
+fi
