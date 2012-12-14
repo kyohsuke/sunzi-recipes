5236577ac953fedca269ead2fde33bea55d1f494
Keisuke KAWAHARA <kyohsuke@conafie.jp>
2012-12-14 17:01:52 +0900

    fixing perlbrew

--- debian/makeinstall/perlbrew.sh
+++ debian/makeinstall/perlbrew.sh
@@ -1,11 +1,11 @@
 # perlbrew
 
-if [ -e "$HOME/perl5/perlbrew/etc/bashrc" ]; then
+if [ -e "$HOME/perl5/perlbrew/etc/bashrc" ]; then                                                                                                                                                       
   echo "perlbrew already installed"
 else
-  # install perlbrew
+  install perlbrew
   wget --no-check-certificate -O - http://install.perlbrew.pl | bash
   yes '' | cpan App::perlbrew
-  perlbrew init
+  echo "source $HOME/perl5/perlbrew/etc/bashrc && perlbrew init" | bash
   echo '[[ -s $HOME/perl5/perlbrew/etc/bashrc ]] && source $HOME/perl5/perlbrew/etc/bashrc' >> ~/.bash_profile
 fi
